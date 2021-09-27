6dd831e14ca29531a92723198960b18c0028d66d
Uday Ghulaxe <udayghulaxe@gmail.com>
2021-09-28 00:41:33 +0530

    Added basic first routing

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,46 @@
 import './App.css';
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
 import { Container, Box } from '@mui/material';
 
 import GlobalHeader from '../GlobalHeader/GlobalHeader';
 
 function App() {
   return (
-    <div className="App">
+    <div>
+        <Router>
         <GlobalHeader/>
-
-        <Container maxWidth="sm">
-          <Box m="auto" textAlign="center">
-            <h1>Hello World</h1>  
-          </Box> 
-        </Container>
+          <div>
+            <Switch>
+              <Route path="/contact">
+                <Container maxWidth="sm">
+                  <Box m="auto" textAlign="center">
+                    <h1>Contact</h1>  
+                  </Box> 
+                </Container>
+              </Route>
+              <Route path="/about">
+                <Container maxWidth="sm">
+                  <Box m="auto" textAlign="center">
+                    <h1>About</h1>  
+                  </Box> 
+                </Container>
+              </Route>
+              <Route path="/">
+                <Container maxWidth="sm">
+                  <Box m="auto" textAlign="center">
+                    <h1>Home</h1>  
+                  </Box> 
+                </Container>
+              </Route>
+            </Switch>
+          </div>
+        </Router>
     </div>
   );
 }
--- src/components/GlobalHeader/GlobalHeader.js
+++ src/components/GlobalHeader/GlobalHeader.js
@@ -5,44 +5,56 @@ import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
 import logo from '../../logo.svg';
 import './GlobalHeader.css'
+import {
+    NavLink
+  } from "react-router-dom";
 
 const GlobalHeader = () => {
     return (
         <Box sx={{ flexGrow: 1 }}>
-            <AppBar elevation={1} className="global-header" color="transparent" position="static">
+            <AppBar elevation={0} className="global-header" color="transparent" position="static">
                 <Toolbar>
                     <img src={logo} className="header-logo" alt="Resume Builder"/>
                     <div className="header-logo-text">
                         <span>Resume Builder</span>
                     </div>
-                    <Button 
-                        variant="text"
-                        color="primary" 
-                        disableElevation
-                        className="mobile-d-none header-menu-link">
-                            Home
-                    </Button>
-                    <Button 
-                        variant="text"
-                        color="primary" 
-                        disableElevation
-                        className="mobile-d-none header-menu-link">
-                            About
-                    </Button>
-                    <Button 
-                        variant="text"
-                        color="primary" 
-                        disableElevation
-                        className="mobile-d-none header-menu-link">
-                            Contact
-                    </Button>
-                    <Button 
-                        variant="contained"
-                        color="primary" 
-                        disableElevation
-                        className="header-login-button">
-                            Create Resume
-                    </Button>
+                    <nav>
+                        <Button
+                            component={NavLink}
+                            to="/"
+                            exact
+                            variant="text"
+                            color="primary" 
+                            disableElevation
+                            className="mobile-d-none header-menu-link">
+                                Home
+                        </Button>
+                        <Button 
+                            component={NavLink}
+                            to="/about"
+                            variant="text"
+                            color="primary" 
+                            disableElevation
+                            className="mobile-d-none header-menu-link">
+                                About
+                        </Button>
+                        <Button
+                            component={NavLink}
+                            to="/contact"
+                            variant="text"
+                            color="primary" 
+                            disableElevation
+                            className="mobile-d-none header-menu-link">
+                                Contact
+                        </Button>
+                        <Button 
+                            variant="contained"
+                            color="primary" 
+                            disableElevation
+                            className="header-login-button">
+                                Create Resume
+                        </Button>
+                    </nav>
                 </Toolbar>
             </AppBar>
         </Box>
