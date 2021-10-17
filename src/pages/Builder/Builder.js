9b973c2dc3b7b312122043a5691ab656241d60e9
Suraj Patil <psuraj787@gmail.com>
2021-10-17 14:42:36 +0530

    Added Achievement Component

--- src/pages/Builder/Builder.js
+++ src/pages/Builder/Builder.js
@@ -7,6 +7,7 @@ import BasicInfo from "../../components/BasicInfo/BasicInfo";
 import Education from "../../components/Education/Education";
 import Experience from "../../components/Experience/Experience";
 import Skills from "../../components/Skills/Skills";
+import Achievement from "../../components/Achievements/Achievement";
 
 function Builder() {
   return (
@@ -21,7 +22,7 @@ function Builder() {
                         <Link underline="none" className="builder-header-menu-link" href="#">Education</Link>
                         <Link underline="none" className="builder-header-menu-link" href="#">Skills</Link>
                         <Link underline="none" className="builder-header-menu-link" href="#">Languages</Link>
-                        <Link underline="none" className="builder-header-menu-link" href="#">Awards</Link>
+                        <Link underline="none" className="builder-header-menu-link" href="#">Achievement</Link>
                       </div>
                   </Toolbar>
               </AppBar>
@@ -33,6 +34,7 @@ function Builder() {
                     <Education></Education>
                     <Experience></Experience>
                     <Skills></Skills>
+                    <Achievement></Achievement>
                 </div>
               </Paper>
           </div>
