0e79ab56cd9aeea220aa8dc6032704e7cb12b695
Uday Ghulaxe <udayghulaxe@gmail.com>
2021-10-26 17:18:49 +0530

    Changes for drag and drop component

--- src/pages/Builder/Builder.js
+++ src/pages/Builder/Builder.js
@@ -1,14 +1,8 @@
-import React, {useState, lazy, Suspense} from "react";
+import React, { useState, lazy, Suspense } from "react";
 import logo from '../../logo.svg';
 import './Builder.css'
-import {AppBar, Box, Toolbar, Link, Paper} from '@mui/material';
-import {
-  GridContextProvider,
-  GridDropZone,
-  GridItem,
-  swap,
-  move
-} from "react-grid-dnd";
+import { AppBar, Box, Toolbar, Link, Paper } from '@mui/material';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
 // import BasicInfo from "../../components/BasicInfo/BasicInfo";
 // import Education from "../../components/Education/Education";
@@ -18,40 +12,7 @@ import {
 // import Languages from "../../components/Languages/Language";
 
 function Builder() {
-  const [items, setItems] = React.useState({
-    left: [
-      { id: 1, name: "ben" },
-      { id: 2, name: "joe" },
-      { id: 3, name: "jason" },
-      { id: 4, name: "chris" },
-      { id: 5, name: "heather" },
-      { id: 6, name: "Richard" }
-    ],
-  });
- 
-  function onChange(sourceId, sourceIndex, targetIndex, targetId) {
-    if (targetId) {
-      const result = move(
-        items[sourceId],
-        items[targetId],
-        sourceIndex,
-        targetIndex
-      );
-      return setItems({
-        ...items,
-        [sourceId]: result[0],
-        [targetId]: result[1]
-      });
-    }
- 
-    const result = swap(items[sourceId], sourceIndex, targetIndex);
-    return setItems({
-      ...items,
-      [sourceId]: result
-    });
-  }
-
-  const arr = [
+  const [arr, setItems] = useState([
     {
       name: 'BasicInfo',
       path: 'BasicInfo/BasicInfo'
@@ -72,64 +33,73 @@ function Builder() {
       name: 'Languages',
       path: 'Languages/Languages',
     }
-  ];
+  ]);
 
   function renderLazyComponent(componentPath) {
     return lazy(() => import(`../../components/${componentPath}`));
- }
+  }
+
+  function onDragEnd(result) {
+    console.log(result);
+    const { destination, source, draggableId } = result;
+    if (!destination || (destination.droppableId === source.droppableId && destination.index === source.index)) {
+      return;
+    }
+
+    const newArr = Array.from(arr);
+    newArr.splice(source.index, 1);
+    newArr.splice(destination.index, 0, arr.filter(x => x.name === draggableId)[0]);
+    setItems(newArr);
+  }
 
   return (
     <div className="builder-wrap">
       <Box sx={{ flexGrow: 1 }}>
-          <AppBar elevation={0} className="global-header" color="inherit" position="fixed">
-              <Toolbar>
-                  <img src={logo} className="header-logo" alt="Resume Builder"/>
-                  <div>
-                    <Link underline="none" className="builder-header-menu-link active" href="#">About</Link>
-                    <Link underline="none" className="builder-header-menu-link" href="#">Experience</Link>
-                    <Link underline="none" className="builder-header-menu-link" href="#">Education</Link>
-                    <Link underline="none" className="builder-header-menu-link" href="#">Skills</Link>
-                    <Link underline="none" className="builder-header-menu-link" href="#">Languages</Link>
-                    <Link underline="none" className="builder-header-menu-link" href="#">Achievement</Link>
-                    <Link underline="none" className="builder-header-menu-link" href="#">Languages</Link>
-                  </div>
-              </Toolbar>
-          </AppBar>
+        <AppBar elevation={0} className="global-header" color="inherit" position="fixed">
+          <Toolbar>
+            <img src={logo} className="header-logo" alt="Resume Builder" />
+            <div>
+              <Link underline="none" className="builder-header-menu-link active" href="#">About</Link>
+              <Link underline="none" className="builder-header-menu-link" href="#">Experience</Link>
+              <Link underline="none" className="builder-header-menu-link" href="#">Education</Link>
+              <Link underline="none" className="builder-header-menu-link" href="#">Skills</Link>
+              <Link underline="none" className="builder-header-menu-link" href="#">Languages</Link>
+              <Link underline="none" className="builder-header-menu-link" href="#">Achievement</Link>
+              <Link underline="none" className="builder-header-menu-link" href="#">Languages</Link>
+            </div>
+          </Toolbar>
+        </AppBar>
       </Box>
-          
-      <div className="resume-paper-wrap">
+
+      <DragDropContext onDragEnd={onDragEnd}>
+        <div className="resume-paper-wrap">
           <Paper className="resume-paper" elevation={3} >
-            <div className="resume-paper-content">
-              {/* Lazy Load All components dynamically  */}
-              <Suspense fallback={<div>Loading</div>}>
-                {arr.map((item) => {
-                    const Comp = renderLazyComponent(`${item.path}`);
-                    return <Comp key={item.name}/>
-                  } 
-                )}
-              </Suspense>
-            </div>
+            <Droppable droppableId="columnLeft">
+              {(provided) => (
+                <div ref={provided.innerRef} {...provided.droppableProps} className="resume-paper-content">
+                  <Suspense fallback={<div>Loading</div>}>
+                    {arr.map((item, index) => {
+                      const Comp = renderLazyComponent(`${item.path}`);
+                      return (
+                        <Draggable key={item.name} draggableId={item.name} index={index}>
+                          {(provided, snapshot) => (
+                            <div className={snapshot.isDragging ? 'component-dragging' : 'idel'} ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps} key={item.name}>
+                              <Comp />
+                            </div>
+                          )}
+                        </Draggable>
+                      );
+                    }
+                    )}
+                    {provided.placeholder}
+                  </Suspense>
+                </div>
+              )}
+            </Droppable>
           </Paper>
-      </div>
-{/* 
-          <GridContextProvider onChange={onChange}>
-            <div className="drop-container">
-              <GridDropZone
-                className="dropzone left"
-                id="left"
-                boxesPerRow={4}
-                rowHeight={200}
-              >
-                {items.left.map(item => (
-                  <GridItem key={item.name}>
-                    <div className="grid-item" style={{cursor: 'grab'}}>
-                    <BasicInfo></BasicInfo>
-                    </div>
-                  </GridItem>
-                ))}
-              </GridDropZone>
-            </div>
-          </GridContextProvider> */}
+
+        </div>
+      </DragDropContext>
     </div>
   );
 }
