6105b9b833777f23cb294edcc8d972dfaf195e10
Uday Ghulaxe <udayghulaxe@gmail.com>
2022-03-31 13:47:59 +0530

    Added Rich Text Edior

---
+++ src/globals.js
@@ -0,0 +1,10 @@
+export const richEditorSettings = {
+    toolbar: [
+      [{ 'header': [1, 2, false] }],
+      ['bold', 'italic', 'underline','strike', 'blockquote'],
+      [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
+      [{ 'color': [] }, { 'background': [] }],
+      ['link', 'image'],
+      ['clean']
+    ],
+  };
