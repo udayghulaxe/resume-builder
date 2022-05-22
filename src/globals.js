1029dde5c9bb4598149a8783665cdef777c6e182
Uday Ghulaxe <udayghulaxe@gmail.com>
2022-05-22 21:52:31 +0530

    Live Edit for skills with progress

--- src/globals.js
+++ src/globals.js
@@ -43,7 +43,7 @@ export const initialData = {
                     title: 'Professional Summary',
                     items: [
                         {
-                            summary: 'This is summary body.',
+                            summary: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
                         },
                     ],
                 },
