6f30e192f27af154d1eebc60f938492702233a78
Uday Ghulaxe <udayghulaxe@gmail.com>
2022-05-22 15:01:02 +0530

    Live Edit for Education component

--- src/components/Education/Education.js
+++ src/components/Education/Education.js
@@ -1,4 +1,7 @@
 import React, { useState } from "react";
+import { useDispatch, useSelector } from 'react-redux';
+import { updateOpenEditorName } from '../../reducers/resumeDataSlice';
+
 import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
 import SchoolIcon from '@mui/icons-material/School';
 import EditIcon from '@mui/icons-material/Edit';
@@ -8,47 +11,55 @@ import EducationEditor from "./EducationEditor";
 import './Education.css'
 
 const Education = (props) => {
-    const [open, setOpen] = useState(false);
-    console.log('calling education', open);
+	const [open, setOpen] = useState(false);
+	console.log('calling education', open);
 
-    const  openEditor = () => {
-        setOpen(true);
-    }
+	const [widgetData, setWidgetData] = useState(props.componentItem.componentData);
 
-    return (
-        <div className="resume-section resume-section-education">
-            <div className="resume-section-title">
-                <span>{props.componentItem.componentData.title}</span>
-                <span className="edit-component-icon">
-                    <EditIcon onClick={openEditor} />
-                </span>
-            </div>     
-            <div className="education-item-wrap">
-                {props.componentItem.componentData.items.map((item, index) => {
-                    return (
-                        <div key={index} className="education-item">
-                            <div className="resume-section-subtitle">
-                                <span className="education-title full-width-field">{item.title}</span>
-                                <span className="education-university full-width-field">{item.university}</span>
-                            </div>
-                            <div className="section-meta">
-                                {item.date && <div className="section-meta-item">
-                                    <CalendarTodayIcon className="section-meta-icon resume-section-body"></CalendarTodayIcon> 
-                                    <div className="resume-section-body" component="span" sx={{ pl: 0.8}}>{item.date}</div>
-                                </div>}
-                                {item.gpa && <div className="section-meta-item">
-                                    <SchoolIcon className="section-meta-icon resume-section-body"></SchoolIcon>
-                                    <div className="resume-section-body" component="span" sx={{ pl: 0.8}}>{item.gpa}</div>
-                                </div>}
-                            </div>
-                        </div>
-                    );
-                })}
-            </div>
+	const dispatch = useDispatch();
+	const openEditorName = useSelector(state => state.resumeDataReducer.openEditorName);
 
-            <EducationEditor open={open} setOpen={setOpen} componentColumn={props.componentColumn} componentName={props.componentItem.name} editorData={props.componentItem.componentData} />
-        </div>
-    );
+	const openEditor = () => {
+		dispatch(updateOpenEditorName(props.componentItem.name));
+		setOpen(true);
+	}
+
+	return (
+		<div className="resume-section resume-section-education">
+			<div className="resume-section-title">
+				<span>{widgetData.title}</span>
+				<span className="edit-component-icon">
+					<EditIcon onClick={openEditor} />
+				</span>
+			</div>
+			<div className="education-item-wrap">
+				{widgetData.items.map((item, index) => {
+					return (
+						<div key={index} className="education-item">
+							<div className="resume-section-subtitle">
+								<span className="education-title full-width-field">{item.title}</span>
+								<span className="education-university full-width-field">{item.university}</span>
+							</div>
+							<div className="section-meta">
+								{item.date && <div className="section-meta-item">
+									<CalendarTodayIcon className="section-meta-icon resume-section-body"></CalendarTodayIcon>
+									<div className="resume-section-body" component="span" sx={{ pl: 0.8 }}>{item.date}</div>
+								</div>}
+								{item.gpa && <div className="section-meta-item">
+									<SchoolIcon className="section-meta-icon resume-section-body"></SchoolIcon>
+									<div className="resume-section-body" component="span" sx={{ pl: 0.8 }}>{item.gpa}</div>
+								</div>}
+							</div>
+						</div>
+					);
+				})}
+			</div>
+
+			{
+				openEditorName === props.componentItem.name ? <EducationEditor setWidgetData={setWidgetData} open={open} setOpen={setOpen} componentColumn={props.componentColumn} componentName={props.componentItem.name} editorData={widgetData} /> : null
+			}
+		</div>
+	);
 }
 
-export default  React.memo(Education);
+export default React.memo(Education);
